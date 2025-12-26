46749c824ae7f541a99de1c0fd832e177e871557
sakilhassan <uboruto9173@gmail.com>
2025-12-26 16:21:25 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 console.log("Website loaded successfully");
+const toggleBtn = document.getElementById("darkToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
